e9b1a2e96e9acd878ea3bace070e13596dae8782
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-09-28 20:44:16 -0300

    FIX: frontend

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,10 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Obtendo __dirname no ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
