e14e059b5586bd739d43e505cdbe1e93bcaa4807
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-09-28 20:47:26 -0300

    FIX: frontend

--- frontend/server.js
+++ frontend/server.js
@@ -1,18 +1,19 @@
-import express from "express";
-import path from "path";
 import { fileURLToPath } from "url";
+import path from "path";
+import express from "express";
 
-// Obtendo __dirname no ES Module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const indexPath = path.join(__dirname, "dist", "index.html");
+
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.use((req, res) => {
+  res.sendFile(indexPath);
 });
 
 app.listen(PORT, () => {
